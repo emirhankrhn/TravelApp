3ba557724db7d3785b9a55ab246cf68e7be5e48c
emirhankrhn <emirhankrhn07@gmail.com>
2025-12-01 12:54:44 +0300

    ekranlar arasında veri aktarmak için route params ekleme (Bölüm 3)

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Button, Text, View } from 'react-native'
 
-const DetailsScreen = ({navigation}) => {
+const DetailsScreen = ({route, navigation}) => {
+
+    const { place, rating } = route.params || {};
+    
   return (
     <View>
       <Text>Details Screen</Text>
+      <Text>Welcome To {place ? place : 'N/A'}</Text>
+      {rating === 5 ? (
+        <Text >⭐⭐⭐⭐⭐ Bu yer mükemmel! 5 yıldız!</Text>
+      ) : (
+        <Text >⭐⭐⭐⭐ Güzel ama mükemmel değil. {rating} yıldız.</Text>
+      )}
       <Button title="Go to Home" onPress={() => navigation.goBack()}/>
     </View>
   )
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,7 +6,14 @@ const HomeScreen = ({navigation}) => {
   return (
     <View>
       <Text>Home Screen</Text>
-     <Button title="Go to Details" onPress={() => navigation.navigate('Details')}/>
+      <Button title="Go to Details" onPress={() => navigation.navigate('Details')}/>
+
+      <Button title="Go to Paris" 
+        onPress={() => navigation.navigate('Details', 
+        { place: 'Paris', rating: 5 })}/>
+      <Button title="Go to London" 
+        onPress={() => navigation.navigate('Details', 
+        { place: 'London', rating: 4 })}/>
     </View>
   )
 }
