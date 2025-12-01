7ca9dc4088bd3731938ecd0d2bc68dcb29db70f8
emirhankrhn <emirhankrhn07@gmail.com>
2025-12-01 13:05:30 +0300

    Explore ve Settings tabları ile bottom tab navigator uygulaması (Bölüm 4)

--- App.js
+++ App.js
@@ -4,21 +4,31 @@ import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SettingsScreen from './screens/SettingScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+}
+
 
 export default function App() {
 
 return (
 <NavigationContainer>
-  <View style={{ justifyContent: 'center', alignItems:'center' }}>
-    <Text>Navigation Ready!</Text>
-  </View>
-  <Stack.Navigator initialRouteName="Home">
-    <Stack.Screen name="Home" component={HomeScreen} />
-    <Stack.Screen name="Details" component={DetailsScreen} />
-  </Stack.Navigator>
-  <StatusBar style="auto" />
-</NavigationContainer>
+      {/* 3) Tab Navigator uygulandı */}
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Explore" component={HomeStack} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+  </NavigationContainer>
 )
 }
